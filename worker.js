eff766548c52cbaf3e2a1eed0d8216bfa565df19
lorenamendesrot-coder <lorenamendesrot@gmail.com>
2026-05-29 02:13:53 -0300

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,29 @@
+import { onRequest as adminMedia }   from "./functions/api/admin-media.js";
+import { onRequest as adminProfile } from "./functions/api/admin-profile.js";
+import { onRequest as createToken }  from "./functions/api/create-token.js";
+import { onRequest as getContent }   from "./functions/api/get-content.js";
+import { onRequest as pixCashin }    from "./functions/api/pix-cashin.js";
+import { onRequest as pixWebhook }   from "./functions/api/pix-webhook.js";
+import { onRequest as syncpayCashin} from "./functions/api/syncpay-cashin.js";
+
+const ROUTES = {
+  "/api/admin-media":    adminMedia,
+  "/api/admin-profile":  adminProfile,
+  "/api/create-token":   createToken,
+  "/api/get-content":    getContent,
+  "/api/pix-cashin":     pixCashin,
+  "/api/pix-webhook":    pixWebhook,
+  "/api/syncpay-cashin": syncpayCashin,
+};
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    const handler = ROUTES[url.pathname];
+    if (handler) {
+      return handler({ request, env, ctx });
+    }
+    // Serve static assets
+    return env.ASSETS.fetch(request);
+  }
+};
