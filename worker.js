7352dd18c860d88af55977141f81c2e9fd0b96eb
lorenamendesrot-coder <lorenamendesrot@gmail.com>
2026-05-29 03:39:11 -0300

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,19 +1,21 @@
-import { onRequest as adminMedia }   from "./functions/api/admin-media.js";
-import { onRequest as adminProfile } from "./functions/api/admin-profile.js";
-import { onRequest as createToken }  from "./functions/api/create-token.js";
-import { onRequest as getContent }   from "./functions/api/get-content.js";
-import { onRequest as pixCashin }    from "./functions/api/pix-cashin.js";
-import { onRequest as pixWebhook }   from "./functions/api/pix-webhook.js";
-import { onRequest as syncpayCashin} from "./functions/api/syncpay-cashin.js";
+import { onRequest as adminMedia }     from "./functions/api/admin-media.js";
+import { onRequest as adminProfile }   from "./functions/api/admin-profile.js";
+import { onRequest as createToken }    from "./functions/api/create-token.js";
+import { onRequest as getContent }     from "./functions/api/get-content.js";
+import { onRequest as pixCashin }      from "./functions/api/pix-cashin.js";
+import { onRequest as pixWebhook }     from "./functions/api/pix-webhook.js";
+import { onRequest as syncpayCashin }  from "./functions/api/syncpay-cashin.js";
+import { onRequest as activateAccess } from "./functions/api/activate-access.js";
 
 const ROUTES = {
-  "/api/admin-media":    adminMedia,
-  "/api/admin-profile":  adminProfile,
-  "/api/create-token":   createToken,
-  "/api/get-content":    getContent,
-  "/api/pix-cashin":     pixCashin,
-  "/api/pix-webhook":    pixWebhook,
-  "/api/syncpay-cashin": syncpayCashin,
+  "/api/admin-media":       adminMedia,
+  "/api/admin-profile":     adminProfile,
+  "/api/create-token":      createToken,
+  "/api/get-content":       getContent,
+  "/api/pix-cashin":        pixCashin,
+  "/api/pix-webhook":       pixWebhook,
+  "/api/syncpay-cashin":    syncpayCashin,
+  "/api/activate-access":   activateAccess,
 };
 
 export default {
